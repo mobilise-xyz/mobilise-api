6bc98c31309d6625b652336afc3a3d1213e310f3
William B <wb2117@ic.ac.uk>
2019-05-25 23:00:27 +0100

    Add initial tests for shifts. Update readme.

--- server/config/config.js
+++ server/config/config.js
@@ -8,11 +8,11 @@ module.exports = {
     "dialect": "postgres"
   },
   "test": {
-    "username": "test",
-    "password": "null",
-    "database": "mobilise_test",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
-    "port": 5432,
+    "port": process.env.DB_PORT,
     "dialect": "postgres"
   },
   "production": {
