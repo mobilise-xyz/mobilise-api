db87561448e45161abbef62a498537112db341fe
William B <wb2117@ic.ac.uk>
2019-05-26 15:05:26 +0100

    Remove unimported use of passport.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/login',	passport.authenticate('local', {
-  successRedirect: '/account',
-  failureRedirect: '/login',
-  failureFlash: true
-  }), function(req, res) {
-  if (req.body.remember) {
-    req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // Cookie expires after 30 days
-    } else {
-    req.session.cookie.expires = false; // Cookie expires at end of session
-  }
-  res.redirect('/');
+router.post('/login',	function(req, res, next) {
+  res.send('Login attempt');
 });
 
 module.exports = router;
