6b6261e856a523e421154485544d371661b172e8
Arjun Singh <as15217@ic.ac.uk>
2019-05-27 20:30:12 +0100

    Remove generateUUID function

--- server/seeders/20190526180803-test-users.js
+++ server/seeders/20190526180803-test-users.js
@@ -5,7 +5,6 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('Users', [
         {
-          id: Sequelize.Utils.generateUUID(),
           firstName: 'Test',
           lastName: 'Volunteerson',
           email: 'testvolunteer@testing.com',
@@ -16,7 +15,6 @@ module.exports = {
           updatedAt: '2019-06-06'
         },
         {
-          id: Sequelize.Utils.generateUUID(),
           firstName: 'Test',
           lastName: 'Adminson',
           email: 'testadmin@testing.com',
