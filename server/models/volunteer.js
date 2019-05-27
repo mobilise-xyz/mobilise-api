8a82b73271a4cb130dd4f93f282a41e90e3dda61
Arjun Singh <as15217@ic.ac.uk>
2019-05-27 22:09:41 +0100

    Volunteer belongsTo User

--- server/models/volunteer.js
+++ server/models/volunteer.js
@@ -1,11 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Volunteer = sequelize.define('Volunteer', {
+    id : {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID
+    },
     roles: DataTypes.ARRAY(DataTypes.INTEGER)
   }, {});
   Volunteer.associate = function(models) {
-    // associations can be defined here
-    this.myAssociation = this.belongsTo(models.User);
+    Volunteer.belongsTo(models.User, {
+      foreignKey: {
+        name: 'id',
+        allowNull: false
+      },
+      onDelete: 'CASCADE'
+    });
   };
   return Volunteer;
 };
