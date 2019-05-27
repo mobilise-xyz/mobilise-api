159c5d0954279e87a67e372e71476ae6c53487fd
Arjun Singh <as15217@ic.ac.uk>
2019-05-27 15:57:29 +0100

    add start end time, postcode

--- server/models/shift.js
+++ server/models/shift.js
@@ -2,7 +2,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Shift = sequelize.define('Shift', {
     title: DataTypes.STRING,
-    description: DataTypes.STRING
+    description: DataTypes.STRING,
+    start: DataTypes.DATE,
+    stop: DataTypes.DATE,
+    postcode: DataTypes.STRING
   }, {});
   Shift.associate = function(models) {
     // associations can be defined here
