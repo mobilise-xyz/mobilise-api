ee713ef813cfd0d7ea7605c4bbff411bfff64350
Arjun Singh <as15217@ic.ac.uk>
2019-05-27 19:46:40 +0100

    Ensure user parameters not null. Use UUID.

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,36 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    admin: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    dob: DataTypes.DATE
+    id : {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    dob: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    }
   }, {});
   User.associate = function(models) {
     // associations can be defined here
