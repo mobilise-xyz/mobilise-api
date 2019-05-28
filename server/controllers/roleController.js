f74c88a6ecaef6f15b0b066995ba25814b6f9d9d
William B <wb2117@ic.ac.uk>
2019-05-28 15:07:48 +0100

    Add role repository. Use repository in controller.

--- server/controllers/roleController.js
+++ server/controllers/roleController.js
@@ -1,23 +1,22 @@
-const Role = require('../models').Role;
+const roleRepository = require('../repositories/roleRepository');
 
 module.exports = {
   create(req, res) {
     if (!req.user.admin) {
       res.status(401).send({message: "Only admin can add roles"})
     } else {
-      return Role
-      .create({
+      return roleRepository.add({
         name: req.body.name,
         involves: req.body.involves
       })
-      .then((role) => res.status(201).send(role))
-      .catch((error) => res.status(400).send(error));
+      .then(role => res.status(201).send(role))
+      .catch(error => res.status(400).send(error));
     }
   },
   
   list(req, res) {
-      return Role
-      .findAll()
+      return roleRepository
+      .getAll()
       .then((roles) => res.status(200).send(roles))
       .catch((error) => res.status(400).send(error));
   },
---
+++ server/repositories/roleRepository.js
@@ -0,0 +1,25 @@
+const Role = require('../models').Role;
+const Q = require('q');
+
+module.exports = {
+  add: function(role) {
+    var deferred = Q.defer();
+    Role
+    .create({
+        name: role.name,
+        involves: role.involves
+    })
+    .then(role => deferred.resolve(role))
+    .catch(error => deferred.reject(error));
+    return deferred.promise;
+  },
+
+  getAll: function()  {
+    var deferred = Q.defer();
+    Role
+    .findAll()
+    .then(roles => deferred.resolve(roles))
+    .catch(error => deferred.reject(error));
+    return deferred.promise;
+  },
+};
