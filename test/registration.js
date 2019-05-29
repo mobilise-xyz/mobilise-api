ea51dd113fb672f0a3b5bc5212cd6792fbe43d5c
Arjun Singh <as15217@ic.ac.uk>
2019-05-29 16:54:24 +0100

    Refactor test folder. Split into multiple files.

---
+++ test/registration.js
@@ -0,0 +1,72 @@
+var request = require('supertest');
+var app = require('../app');
+var chai = require('chai');
+
+var expect = chai.expect;
+
+describe('Register Volunteer', function() {
+
+  it('Can register a volunteer', function(done) {
+    request(app)
+      .post('/auth/register')
+      .send(
+        {
+          firstName: 'Volun',
+          lastName: 'Teer',
+          email: 'volunteer@testing.com',
+          password: 'Volunteer123',
+          dob: '1998-11-25'
+        }
+      )
+      .set('Accept', 'application/json')
+      .expect(201)
+      .end(function(error, response) {
+        if (error) {
+          done(error);
+        }
+
+        expect(response.body.email).to.equal('volunteer@testing.com');
+        expect(response.body.firstName).to.equal('Volun');
+        expect(response.body.lastName).to.equal('Teer');
+        expect(response.body.dob).to.equal('1998-11-25');
+        expect(response.body.admin).to.equal(false);
+        
+        done();
+      })
+  });
+
+})
+
+describe('Register Admin', function() {
+
+  it('Can register an admin', function(done) {
+    request(app)
+      .post('/auth/register')
+      .send(
+        {
+          firstName: 'Ad',
+          lastName: 'Min',
+          email: 'admin@testing.com',
+          password: 'Admin123',
+          dob: '1998-11-25',
+          admin: 'true'
+        }
+      )
+      .set('Accept', 'application/json')
+      .expect(201)
+      .end(function(error, response) {
+        if (error) {
+          done(error);
+        }
+
+        expect(response.body.email).to.equal('admin@testing.com');
+        expect(response.body.firstName).to.equal('Ad');
+        expect(response.body.lastName).to.equal('Min');
+        expect(response.body.dob).to.equal('1998-11-25');
+        expect(response.body.admin).to.equal(true);
+        
+        done();
+      })
+  });
+
+})
