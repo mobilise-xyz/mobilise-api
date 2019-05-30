fcbdf2cce0bf05bbe5401943e291485d571f3a95
Arjun Singh <as15217@ic.ac.uk>
2019-05-30 22:27:30 +0100

    Prettify shifts tests

--- test/shifts.js
+++ test/shifts.js
@@ -4,6 +4,90 @@ var chai = require('chai');
 
 var expect = chai.expect;
 
+describe('Add shifts', function() {
+
+  it('Does not allow unauthorised request to add shift', function(done) {
+    request(app)
+      .post('/shifts')
+      .send(
+        {
+          title: 'Shift Title',
+          description: 'Shift Description',
+          date: '2019-02-08',
+          start: '16:00',
+          stop: '18:00',
+          postcode: 'SW72AZ'
+        }
+      )
+      .set('Accept', 'application/json')
+      .expect(401, done);
+  });
+  
+  it('Does not allow non-admin to add shift', function(done){
+    // Acquire bearer token
+    request(app)
+      .post('/auth/login')
+      .send(
+        {
+          email: 'testvolunteer@testing.com',
+          password: 'Volunteer123'
+        }
+      )
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(response => {
+        // Use bearer token to add shift
+        request(app)
+        .post('/shifts')
+        .send(
+          {
+            title: 'Shift Title',
+            description: 'Shift Description',
+            date: '2019-02-08',
+            start: '16:00',
+            stop: '18:00',
+            postcode: 'SW72AZ'
+          }
+        )
+        .set('Authorization', 'Bearer '+response.body.token)
+        .set('Accept', 'application/json')
+        .expect(401, done);
+      });
+  }) 
+
+  it('Allows admin to add shift', function(done){
+    // Acquire bearer token
+    request(app)
+      .post('/auth/login')
+      .send(
+        {
+          email: 'testadmin@testing.com',
+          password: 'Admin123'
+        }
+      )
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(response => {
+        // Use bearer token to add shift
+        request(app)
+        .post('/shifts')
+        .send(
+          {
+            title: 'Shift Title',
+            description: 'Shift Description',
+            date: '2019-02-08',
+            start: '16:00',
+            stop: '18:00',
+            postcode: 'SW72AZ'
+          }
+        )
+        .set('Authorization', 'Bearer '+response.body.token)
+        .set('Accept', 'application/json')
+        .expect(201, done);
+      });
+  })
+})
+
 describe('Retrieve shifts', function() {
 
   it('Does not allow unauthorised requests to get shifts', function(done) {
@@ -44,87 +128,4 @@ describe('Retrieve shifts', function() {
       });
   });
 
-})
-
-describe('Add shifts', function() {
-  it('Does not allow unauthorised request to add shift', function(done) {
-    request(app)
-      .post('/shifts')
-      .send(
-        {
-          title: 'Food pickup',
-            description: 'I mean its pretty self explanatory mate',
-            date: '2019-02-08',
-            start: '16:00',
-            stop: '18:00',
-            postcode: 'SW72AZ'
-        }
-      )
-      .set('Accept', 'application/json')
-      .expect(401, done);
-  });
-  
-  it('Does not allow non-admin to add shift', function(done){
-    // Acquire bearer token
-    request(app)
-      .post('/auth/login')
-      .send(
-        {
-          email: 'testvolunteer@testing.com',
-          password: 'Volunteer123'
-        }
-      )
-      .set('Accept', 'application/json')
-      .expect(200)
-      .then(response => {
-        // Use bearer token to add shift
-        request(app)
-        .post('/shifts')
-        .send(
-          {
-            title: 'Food pickup',
-            description: 'I mean its pretty self explanatory mate',
-            date: '2019-02-08',
-            start: '16:00',
-            stop: '18:00',
-            postcode: 'SW72AZ'
-          }
-        )
-        .set('Authorization', 'Bearer '+response.body.token)
-        .set('Accept', 'application/json')
-        .expect(401, done);
-      });
-  }) 
-
-  it('Allows admin to add shift', function(done){
-    // Acquire bearer token
-    request(app)
-      .post('/auth/login')
-      .send(
-        {
-          email: 'testadmin@testing.com',
-          password: 'Admin123'
-        }
-      )
-      .set('Accept', 'application/json')
-      .expect(200)
-      .then(response => {
-        // Use bearer token to add shift
-        request(app)
-        .post('/shifts')
-        .send(
-          {
-            title: 'Food pickup',
-            description: 'I mean its pretty self explanatory mate',
-            date: '2019-02-08',
-            start: '16:00',
-            stop: '18:00',
-            postcode: 'SW72AZ'
-          }
-        )
-        .set('Authorization', 'Bearer '+response.body.token)
-        .set('Accept', 'application/json')
-        .expect(201, done);
-      });
-  })
 })
