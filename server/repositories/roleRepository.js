326856d8b9006c75dfddf37dc86e61db9ce0b104
Arjun Singh <as15217@ic.ac.uk>
2019-05-31 12:37:10 +0100

    Use interface in role repository

--- server/repositories/roleRepository.js
+++ server/repositories/roleRepository.js
@@ -1,34 +1,38 @@
 const Role = require('../models').Role;
 const Q = require('q');
 
-module.exports = {
-  add: function(role) {
-    var deferred = Q.defer();
-    Role
-    .create({
-        name: role.name,
-        involves: role.involves
-    })
-    .then(role => deferred.resolve(role))
-    .catch(error => deferred.reject(error));
-    return deferred.promise;
-  },
+const RoleRepositoryInterface = require('./roleRepositoryInterface')
 
-  getByName: function(name) {
-    var deferred = Q.defer();
-    Role
-    .findOne({where: {name: name}})
-    .then(role => deferred.resolve(role))
-    .catch(error => deferred.reject(error));
-    return deferred.promise;
-  },
+var RoleRepository = Object.create(RoleRepositoryInterface);
 
-  getAll: function()  {
-    var deferred = Q.defer();
-    Role
-    .findAll()
-    .then(roles => deferred.resolve(roles))
-    .catch(error => deferred.reject(error));
-    return deferred.promise;
-  },
-};
+RoleRepository.add = function(role) {
+  var deferred = Q.defer();
+  Role
+  .create({
+      name: role.name,
+      involves: role.involves
+  })
+  .then(role => deferred.resolve(role))
+  .catch(error => deferred.reject(error));
+  return deferred.promise;
+}
+
+RoleRepository.getByName = function(name) {
+  var deferred = Q.defer();
+  Role
+  .findOne({where: {name: name}})
+  .then(role => deferred.resolve(role))
+  .catch(error => deferred.reject(error));
+  return deferred.promise;
+}
+
+RoleRepository.getAll = function()  {
+  var deferred = Q.defer();
+  Role
+  .findAll()
+  .then(roles => deferred.resolve(roles))
+  .catch(error => deferred.reject(error));
+  return deferred.promise;
+}
+
+module.exports = RoleRepository;
