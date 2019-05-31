9657eb5e8fce45d0dafd25d100ccaccffc0a40b2
Arjun Singh <as15217@ic.ac.uk>
2019-05-31 15:58:11 +0100

    Use interface for user repo

--- server/repositories/userRepository.js
+++ server/repositories/userRepository.js
@@ -1,38 +1,41 @@
 const User = require('../models').User;
 const Q = require('q');
+const UserRepositoryInterface = require('./interfaces/userRepositoryInterface');
 
-module.exports = {
-  add: function(user, hash) {
-    var deferred = Q.defer();
-    User
-    .create({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        password: hash,
-        admin: user.admin,
-        dob: user.dob
-    })
-    .then(user => deferred.resolve(user))
-    .catch(error => deferred.reject(error));
-    return deferred.promise;
-  },
+var UserRepository = Object.create(UserRepositoryInterface);
 
-  getByEmail: function(email) {
-    var deferred = Q.defer();
-    User
-    .findOne({where: {email: email}})
-    .then(user => deferred.resolve(user))
-    .catch(error => deferred.reject(error));
-    return deferred.promise;
-  },
-
-  getById: function(id) {
-    var deferred = Q.defer();
-    User
-    .findOne({where: {id: id}})
-    .then(user => deferred.resolve(user))
-    .catch(error => deferred.reject(error));
-    return deferred.promise;
-  }
+UserRepository.add = function(user, hash) {
+  var deferred = Q.defer();
+  User
+  .create({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      password: hash,
+      admin: user.admin,
+      dob: user.dob
+  })
+  .then(user => deferred.resolve(user))
+  .catch(error => deferred.reject(error));
+  return deferred.promise;
 };
+
+UserRepository.getByEmail = function(email) {
+  var deferred = Q.defer();
+  User
+  .findOne({where: {email: email}})
+  .then(user => deferred.resolve(user))
+  .catch(error => deferred.reject(error));
+  return deferred.promise;
+};
+
+UserRepository.getById = function(id) {
+  var deferred = Q.defer();
+  User
+  .findOne({where: {id: id}})
+  .then(user => deferred.resolve(user))
+  .catch(error => deferred.reject(error));
+  return deferred.promise;
+};
+
+module.exports = UserRepository;
