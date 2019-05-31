8ba8278642fa2f7c1d2e5b7aa7b54b17a6fe85ce
Arjun Singh <as15217@ic.ac.uk>
2019-05-31 16:01:56 +0100

    Use interface for shift repo

---
+++ server/repositories/interfaces/shiftRepositoryInterface.js
@@ -0,0 +1,8 @@
+var ShiftRepositoryInterface = {
+    add: function(shift, id) {},
+    getAllWithRoles: function() {},
+    getAll: function(attributes) {},
+    removeById: function(id) {}
+}
+
+module.exports = ShiftRepositoryInterface;
--- server/repositories/shiftRepository.js
+++ server/repositories/shiftRepository.js
@@ -1,50 +1,53 @@
 const Shift = require('../models').Shift;
 const Q = require('q');
 const sequelize = require('sequelize')
+const ShiftRepositoryInterface = require('./interfaces/shiftRepositoryInterface');
 
-module.exports = {
-  add: function(shift, id) {
-    var deferred = Q.defer();
-    Shift
-    .create({
-      title: shift.title,
-      creatorId: id,
-      description: shift.description,
-      date: shift.date,
-      start: shift.start,
-      stop: shift.stop,
-      address: shift.address
-    })
-    .then(result => deferred.resolve(result))
-    .catch(error => deferred.reject(error));
-    return deferred.promise;
-  },
+var ShiftRepository = Object.create(ShiftRepositoryInterface);
 
-  getAllWithRoles: function() {
-    var deferred = Q.defer();
-    Shift.findAll({include: ['roles'], order: [
-      [sequelize.literal('date, start'), 'asc']
-    ]})
-    .then(shifts => deferred.resolve(shifts))
-    .catch(err => deferred.resolve(err));
-    return deferred.promise;
-  },
-
-  getAll: function(attributes) {
-    var deferred = Q.defer();
-    Shift.findAll({attributes: attributes, order: [
-      [sequelize.literal('date, start'), 'asc']
-    ]})
-    .then(shifts => deferred.resolve(shifts))
-    .catch(err => deferred.resolve(err));
-    return deferred.promise;
-  },
-
-  removeById: function(id) {
-    var deferred = Q.defer();
-    Shift.destroy({where: {id: id}})
-    .then(shift => deferred.resolve(shift))
-    .catch(err => deferred.resolve(err));
-    return deferred.promise;
-  }
+ShiftRepository.add = function(shift, id) {
+  var deferred = Q.defer();
+  Shift
+  .create({
+    title: shift.title,
+    creatorId: id,
+    description: shift.description,
+    date: shift.date,
+    start: shift.start,
+    stop: shift.stop,
+    address: shift.address
+  })
+  .then(result => deferred.resolve(result))
+  .catch(error => deferred.reject(error));
+  return deferred.promise;
 };
+
+ShiftRepository.getAllWithRoles = function() {
+  var deferred = Q.defer();
+  Shift.findAll({include: ['roles'], order: [
+    [sequelize.literal('date, start'), 'asc']
+  ]})
+  .then(shifts => deferred.resolve(shifts))
+  .catch(err => deferred.resolve(err));
+  return deferred.promise;
+};
+
+ShiftRepository.getAll = function(attributes) {
+  var deferred = Q.defer();
+  Shift.findAll({attributes: attributes, order: [
+    [sequelize.literal('date, start'), 'asc']
+  ]})
+  .then(shifts => deferred.resolve(shifts))
+  .catch(err => deferred.resolve(err));
+  return deferred.promise;
+};
+
+ShiftRepository.removeById = function(id) {
+  var deferred = Q.defer();
+  Shift.destroy({where: {id: id}})
+  .then(shift => deferred.resolve(shift))
+  .catch(err => deferred.resolve(err));
+  return deferred.promise;
+}
+
+module.exports = ShiftRepository;
