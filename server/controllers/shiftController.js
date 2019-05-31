cb030ed2d4b173b90ed3de3d10cf8d212a670d4e
William B <wb2117@ic.ac.uk>
2019-05-31 15:04:54 +0100

    Implement repeating shifts.

--- server/controllers/shiftController.js
+++ server/controllers/shiftController.js
@@ -1,5 +1,6 @@
 const shiftRepository = require('../repositories').ShiftRepository;
 const roleRepository = require('../repositories').RoleRepository;
+const moment = require('moment');
 
 module.exports = {
   async create(req, res) {
@@ -30,22 +31,59 @@ module.exports = {
       if (errs.length > 0) {
         res.status(400).send({"Could not add shift" : errs});
       } else {
-        // Add the shift
-        shiftRepository
-        .add(req.body, req.user.id)
-        .then(async(shift) => {
-          // Add the roles to shift
-          var i;
-          for (i = 0; i < roles.length; i++) {
-            await shift.addRole(roles[i], {through: {numberRequired: numbers[i]}})
+        // Check if repeated
+        if (req.body.isRepeating) {
+          const repeatedTypes = {"weekly": 7, "daily": 1};
+          var increment = repeatedTypes[req.body.repeatedType];
+          // Check if valid request
+          if (increment) {
+            // Create repeated shift
+            var successful = true;
+            var startDate = moment(req.body.date,"YYYY-MM-DD");
+            var untilDate =  moment(req.body.untilDate,"YYYY-MM-DD");
+            while (moment(startDate).isBefore(untilDate) && successful) {
+              // Add the shift
+              req.body.date = startDate;
+              await shiftRepository
+              .add(req.body, req.user.id)
+              .then(async(shift) => {
+                // Add the roles to shift
+                var i;
+                for (i = 0; i < roles.length; i++) {
+                  await shift.addRole(roles[i], {through: {numberRequired: numbers[i]}})
+                }
+                return shift;
+              })
+              .catch(err => {
+                successful = false;
+                res.status(500).send(err);
+              });
+              startDate = moment(startDate).add(increment, 'd').toDate()
+            }
+            if (successful) {
+              res.status(200).send({message: "Created recurring shift"})
+            }
+          } else {
+            res.status(400).send({message: "Invalid repeated type, must be weekly or daily."});
           }
-          return shift;
-        })
-        .then(result => {
-          res.status(201).send(result);
-        })
-        .catch(err => res.status(500).send(err))
+        } else {
+          // Add the shift
+          shiftRepository
+          .add(req.body, req.user.id)
+          .then(async(shift) => {
+            // Add the roles to shift
+            var i;
+            for (i = 0; i < roles.length; i++) {
+              await shift.addRole(roles[i], {through: {numberRequired: numbers[i]}})
+            }
+            return shift;
+          })
+          .then(result => {
+            res.status(201).send(result);
+          })
+          .catch(err => res.status(500).send(err));
         }
+      }
     }
   },
   
