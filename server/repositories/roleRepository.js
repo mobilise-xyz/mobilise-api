eb7fe0c21d5156e33ae09424affc4f1e45244567
Arjun Singh <as15217@ic.ac.uk>
2019-05-31 13:58:05 +0100

    Add removeByName function to role repo

--- server/repositories/roleRepository.js
+++ server/repositories/roleRepository.js
@@ -7,31 +7,48 @@ var RoleRepository = Object.create(RoleRepositoryInterface);
 
 RoleRepository.add = function(role) {
   var deferred = Q.defer();
+  
   Role
-  .create({
-      name: role.name,
-      involves: role.involves
-  })
-  .then(role => deferred.resolve(role))
-  .catch(error => deferred.reject(error));
+    .create({
+        name: role.name,
+        involves: role.involves
+    })
+    .then(role => deferred.resolve(role))
+    .catch(error => deferred.reject(error));
+  
   return deferred.promise;
 }
 
 RoleRepository.getByName = function(name) {
   var deferred = Q.defer();
+  
   Role
-  .findOne({where: {name: name}})
-  .then(role => deferred.resolve(role))
-  .catch(error => deferred.reject(error));
+    .findOne({where: {name: name}})
+    .then(role => deferred.resolve(role))
+    .catch(error => deferred.reject(error));
+  
   return deferred.promise;
 }
 
 RoleRepository.getAll = function()  {
   var deferred = Q.defer();
+  
   Role
-  .findAll()
-  .then(roles => deferred.resolve(roles))
-  .catch(error => deferred.reject(error));
+    .findAll()
+    .then(roles => deferred.resolve(roles))
+    .catch(error => deferred.reject(error));
+  
+  return deferred.promise;
+}
+
+RoleRepository.removeByName = function(name) {
+  var deferred = Q.defer();
+
+  Role
+    .destroy({where: {name: name}})
+    .then(role => deferred.resolve(role))
+    .catch(error => deferred.reject(error))
+    
   return deferred.promise;
 }
 
