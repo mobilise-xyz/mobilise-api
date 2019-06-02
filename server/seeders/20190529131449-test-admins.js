8b221669bad706e46a4bf7e84ad221cf072f7361
Arjun Singh <as15217@ic.ac.uk>
2019-06-02 16:11:28 +0100

    Ensure only seeded admin entry is removed

--- server/seeders/20190529131449-test-admins.js
+++ server/seeders/20190529131449-test-admins.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var userId = '8fa1b90c-80b6-11e9-bc42-526af7764f64';
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Admins', [
       {
-        userId: '8fa1b90c-80b6-11e9-bc42-526af7764f64',
+        userId: userId,
         createdAt: '2019-06-06',
         updatedAt: '2019-06-06'
       }
@@ -12,6 +14,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Admins', null, {});
+    return queryInterface.bulkDelete('Admins', {userId : userId}, {});
   }
 };
