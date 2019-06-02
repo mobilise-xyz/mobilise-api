ba5108a08ac6882abf7f76c7339fbd421a16da5c
Arjun Singh <as15217@ic.ac.uk>
2019-06-02 17:07:16 +0100

    Use ids to remove users after testing

--- server/seeders/20190526180803-test-users.js
+++ server/seeders/20190526180803-test-users.js
@@ -1,11 +1,13 @@
 'use strict';
 var bcrypt = require('bcryptjs');
 
+var ids = ['8fa1b3d0-80b6-11e9-bc42-526af7764f65', '8fa1b90c-80b6-11e9-bc42-526af7764f64'];
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.bulkInsert('Users', [
         {
-          id: '8fa1b3d0-80b6-11e9-bc42-526af7764f65',
+          id: ids[0],
           firstName: 'Seeded',
           lastName: 'Volunteerson',
           email: 'testvolunteer@testing.com',
@@ -16,7 +18,7 @@ module.exports = {
           updatedAt: '2019-06-06'
         },
         {
-          id: '8fa1b90c-80b6-11e9-bc42-526af7764f64',
+          id: ids[1],
           firstName: 'Seeded',
           lastName: 'Adminson',
           email: 'testadmin@testing.com',
@@ -31,6 +33,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-      return queryInterface.bulkDelete('Users', {firstName : 'Seeded'}, {});
+      return queryInterface.bulkDelete('Users', {id: ids});
   }
 };
