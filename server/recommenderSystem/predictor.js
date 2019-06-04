db23fcb75780cde4b800aacc812066718319a8ea
Arjun Singh <as15217@ic.ac.uk>
2019-06-04 13:47:35 +0100

    Use recommended shift repo in predictor

--- server/recommenderSystem/predictor.js
+++ server/recommenderSystem/predictor.js
@@ -3,44 +3,57 @@ const RecommendedShift = require('../models').RecommendedShift;
 const sequelize = require("sequelize");
 
 const shiftRepository = require('../repositories').ShiftRepository;
+const recommendedShiftRepository = require('../repositories').RecommendedShiftRepository;
 
-var Predictor = function(shiftRepository) {
+var Predictor = function(shiftRepository, recommendedShiftRepository) {
 
   this.shiftRepository = shiftRepository;
+  this.recommendedShiftRepository = recommendedShiftRepository;
 
   this.computeRecommendedShifts = function() {
 
     // Remove old entries in Recommended Shifts Table
+    recommendedShiftRepository
+      .destroy()
+      .then(_ => {
 
+        shiftRepository
+          .getAllWithRequirements()
+          .then(shifts => {
+            shifts.foreach(shift => {
+              
+              // Obtain the shift requirements
+              var requirements = shift.getRequirements();
 
-    shiftRepository
-      .getAllWithRequirements()
-      .then(shifts => {
-        shifts.foreach(shift => {
-          
-          // Obtain the shift requirements
-          var requirements = shift.getRequirements();
+              // Construct Map of role name to number required
+              var requirementsMap = new Map();
+              requirements.foreach(requirement => {
+                requirementsMap.set(requirement.roleName, requirement.numberRequired);
+              })
 
-          // Construct Map of role name to number required
-          var requirementsMap = new Map();
-          requirements.foreach(requirement => {
-            requirementsMap.set(requirement.roleName, requirement.numberRequired);
-          })
+              // Obtain the bookings made for the shift
+              var bookings = shift.getBookings();
 
-          // Obtain the bookings made for the shift
-          var bookings = shift.getBookings();
+              // Loop through the bookings and update requirementsMap
+              bookings.foreach(booking => {
+                requirementsMap.set(booking.roleName, requirementsMap.get(booking.roleName) - 1);
+              })
 
-          // Loop through the bookings and update requirementsMap
-          bookings.foreach(booking => {
-            requirementsMap.set(booking.roleName, requirementsMap.get(booking.roleName) - 1);
-          })
+              // Add entry in Recommended Shifts Table
+              requirementsMap
+                .keys()
+                .foreach(roleName => {
+                  recommendedShiftRepository.add(
+                    shiftId, 
+                    roleName,
+                    requirementsMap.get(key)
+                  )
+                })
+            })
+          });
 
-          // Add entry in Recommended Shifts Table
-
-        })
-      });
-
+      })
   }
 }
 
-module.exports = new Predictor(shiftRepository);
+module.exports = new Predictor(shiftRepository, recommendedShiftRepository);
