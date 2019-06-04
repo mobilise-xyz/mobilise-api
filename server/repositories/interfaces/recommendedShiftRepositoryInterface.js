10af5a6e8c02d97d4c9c462615ae9881a8542592
Arjun Singh <as15217@ic.ac.uk>
2019-06-04 13:46:55 +0100

    Add interface for recommended shift repo

---
+++ server/repositories/interfaces/recommendedShiftRepositoryInterface.js
@@ -0,0 +1,6 @@
+var RecommendedShiftRepositoryInterface = {
+    destroy: function() {},
+    add: function(shiftId, roleName, expectedShortage) {}
+};
+
+module.exports = RecommendedShiftRepositoryInterface;
