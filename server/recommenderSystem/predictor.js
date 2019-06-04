3b37502be41d5afc8c6061b1f6f90f094dcf5839
Arjun Singh <as15217@ic.ac.uk>
2019-06-04 12:54:27 +0100

    Use get all with requirements

--- server/recommenderSystem/predictor.js
+++ server/recommenderSystem/predictor.js
@@ -13,31 +13,31 @@ var Predictor = function(shiftRepository) {
     var map = new Map();
 
     shiftRepository
-      .getAll([])
+      .getAllWithRequirements()
       .then(shifts => {
-          shifts.foreach(shift => {
-            
-            // Obtain the shift requirements
-            var requirements = shift.getRequirements();
+        shifts.foreach(shift => {
+          
+          // Obtain the shift requirements
+          var requirements = shift.getRequirements();
 
-            // Construct Map of role name to number required
-            var requirementsMap = new Map();
-            requirements.foreach(requirement => {
-              requirementsMap.set(requirement.roleName, requirement.numberRequired);
-            })
-
-            // Obtain the bookings made for the shift
-            var bookings = shift.getBookings();
-
-            // Loop through the bookings and update requirementsMap
-            bookings.foreach(booking => {
-              requirementsMap.set(booking.roleName, requirementsMap.get(booking.roleName) - 1);
-            })
-
-            // Add entry for shift in map
-            map.set(shift.id, requirementsMap);
-
+          // Construct Map of role name to number required
+          var requirementsMap = new Map();
+          requirements.foreach(requirement => {
+            requirementsMap.set(requirement.roleName, requirement.numberRequired);
           })
+
+          // Obtain the bookings made for the shift
+          var bookings = shift.getBookings();
+
+          // Loop through the bookings and update requirementsMap
+          bookings.foreach(booking => {
+            requirementsMap.set(booking.roleName, requirementsMap.get(booking.roleName) - 1);
+          })
+
+          // Add entry for shift in map
+          map.set(shift.id, requirementsMap);
+
+        })
       });
 
       return map;
