dd7fb1e88136b52d150db63d300b35274ae9ce30
Arjun Singh <as15217@ic.ac.uk>
2019-06-06 15:07:52 +0100

    Add tests for users endpoint

--- test/users.js
+++ test/users.js
@@ -12,6 +12,46 @@ describe('Retrieving users', function() {
       .expect(401, done);
   });
 
+  it('Does not allow volunteer to retrieve another user by id', function(done) {
+    request(app)
+      .post('/auth/login')
+      .send(
+          {
+          email: Seeded.volunteers[0].email,
+          password: Seeded.volunteers[0].password
+          }
+      )
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((response) => {
+          request(app)
+          .get(`/users/${Seeded.volunteers[1].UUID}`)
+          .set('Authorization', 'Bearer '+response.body.token)
+          .set('Accept', 'application/json')
+          .expect(401, done);
+      })
+  });
+
+  it('Allows a volunteer to retrieve their own info', function(done) {
+    request(app)
+      .post('/auth/login')
+      .send(
+          {
+          email: Seeded.volunteers[0].email,
+          password: Seeded.volunteers[0].password
+          }
+      )
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((response) => {
+          request(app)
+          .get(`/users/${Seeded.volunteers[0].UUID}`)
+          .set('Authorization', 'Bearer '+response.body.token)
+          .set('Accept', 'application/json')
+          .expect(200, done);
+      })
+  });
+
   it('Admin can retrieve a user by id', function(done) {
     request(app)
       .post('/auth/login')
