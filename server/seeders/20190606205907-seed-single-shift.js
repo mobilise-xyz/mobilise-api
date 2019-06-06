4c2e60a59c63d1d4bc680cbe66fedf5edbd7fb02
Arjun Singh <as15217@ic.ac.uk>
2019-06-06 23:14:56 +0100

    Add seeder for single shifts

---
+++ server/seeders/20190606205907-seed-single-shift.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Seeded = require('../utils/seeded');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Shifts', 
+      [
+        {
+          id: Seeded.shifts[0].UUID,
+          title: Seeded.shifts[0].title,
+          description: Seeded.shifts[0].description,
+          date: Seeded.shifts[0].date,
+          start: Seeded.shifts[0].start,
+          stop: Seeded.shifts[0].stop,
+          address: Seeded.shifts[0].address,
+          creatorId: Seeded.shifts[0].creatorId,
+          createdAt: Seeded.roles[0].createdAt,
+          updatedAt: Seeded.roles[0].updatedAt
+        },
+        {
+          id: Seeded.shifts[1].UUID,
+          title: Seeded.shifts[1].title,
+          description: Seeded.shifts[1].description,
+          date: Seeded.shifts[1].date,
+          start: Seeded.shifts[1].start,
+          stop: Seeded.shifts[1].stop,
+          address: Seeded.shifts[1].address,
+          creatorId: Seeded.shifts[1].creatorId,
+          createdAt: Seeded.roles[0].createdAt,
+          updatedAt: Seeded.roles[0].updatedAt
+        }
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Shifts', {
+      id: [
+        Seeded.shifts[0].UUID,
+        Seeded.shifts[1].UUID
+      ]
+    });
+  }
+};
