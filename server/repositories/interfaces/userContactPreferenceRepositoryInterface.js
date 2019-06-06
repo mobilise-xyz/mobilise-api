7c2e1b8ba7bfddb9d566c9eb083efba720106c75
Arjun Singh <as15217@ic.ac.uk>
2019-06-06 18:09:47 +0100

    Add interface for user contact repo

---
+++ server/repositories/interfaces/userContactPreferenceRepositoryInterface.js
@@ -0,0 +1,7 @@
+var UserContactPreference = {
+    add: function(userId, contactPreference) {},
+    getById: function(userId) {},
+    update: function(userId, contactPreference) {}
+};
+
+module.exports = UserContactPreference;
