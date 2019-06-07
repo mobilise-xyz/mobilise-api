73de5e5f1daf6d10d026b53c15186aea0156d52f
William B <wb2117@ic.ac.uk>
2019-06-07 01:10:45 +0100

    Filter requirements for a booked shift.

--- server/controllers/volunteerController.js
+++ server/controllers/volunteerController.js
@@ -13,10 +13,11 @@ var VolunteerController = function(volunteerRepository, shiftRepository) {
   this.shiftRepository = shiftRepository;
 
   this.list = function(req, res) {
-
     // Restrict access to admin
     if (!req.user.isAdmin) {
-      res.status(401).send({ message: "Only admins can access volunteer catalogue" });
+      res
+        .status(401)
+        .send({ message: "Only admins can access volunteer catalogue" });
       return;
     }
 
@@ -26,112 +27,98 @@ var VolunteerController = function(volunteerRepository, shiftRepository) {
       .catch(err => res.status(500).send(err));
   };
 
-  this.getStats = function(req, res) {
-    
+  (this.getStats = function(req, res) {
     // Check bearer token id matches parameter id
     if (req.user.id != req.params.id) {
-      res
-        .status(401)
-        .send({ message: "You can only view your own stats." });
+      res.status(401).send({ message: "You can only view your own stats." });
       return;
     }
 
     res.status(200).send({
       contributions: {
         shiftsCompleted: 10,
-        hours: 10, 
+        hours: 10,
         challengesCompleted: 5
       }
-    })
-
-  },
-
-  this.getActivity = function(req, res) {
-
-    // Check bearer token id matches parameter id
-    if (req.user.id != req.params.id) {
-      res
-        .status(401)
-        .send({ message: "You can only view your own stats." });
-      return;
-    }
-
-    res.status(200).send({
-      myActivity: [
-        {
-          title: "Title",
-          description: "Description"
-        },
-        {
-          title: "Title",
-          description: "Description"
-        },
-        {
-          title: "Title",
-          description: "Description"
-        },
-        {
-          title: "Title",
-          description: "Description"
-        },
-        {
-          title: "Title",
-          description: "Description"
-        }
-      ]
     });
-
-  },
-
-  this.getHallOfFame = function(req, res) {
-
-    res.status(200).send({
-      hallOfFame: {
-        fastResponder: {
-          name: "Mark Wheelhouse",
-          number: 3
-        },
-        mostHours: {
-          name: "Joon-Ho Son",
-          number: 50
-        },
-        onTheRise: {
-          name: "Jane Doe",
-          number: 4
-        },
-
+  }),
+    (this.getActivity = function(req, res) {
+      // Check bearer token id matches parameter id
+      if (req.user.id != req.params.id) {
+        res.status(401).send({ message: "You can only view your own stats." });
+        return;
       }
-    })
 
-  },
-
-  this.updateAvailability = function(req, res) {
-    // Check bearer token id matches parameter id
-    if (req.user.id != req.params.id) {
-      res
-        .status(401)
-        .send({ message: "You can only update your own availability." });
-      return;
-    }
-
-    volunteerRepository
-      .getById(req.params.id)
-      .then(volunteer => {
-        if (!volunteer) {
-          res.status(400).send({ message: "No volunteer with that id" });
-        } else {
-          volunteerRepository
-            .updateAvailability(req.params.id, req.body.availability)
-            .then(result =>
-              res.status(201).send({
-                message: "Availability Updated Successfuly"
-              })
-            )
-            .catch(error => res.status(400).send(error));
+      res.status(200).send({
+        myActivity: [
+          {
+            title: "Title",
+            description: "Description"
+          },
+          {
+            title: "Title",
+            description: "Description"
+          },
+          {
+            title: "Title",
+            description: "Description"
+          },
+          {
+            title: "Title",
+            description: "Description"
+          },
+          {
+            title: "Title",
+            description: "Description"
+          }
+        ]
+      });
+    }),
+    (this.getHallOfFame = function(req, res) {
+      res.status(200).send({
+        hallOfFame: {
+          fastResponder: {
+            name: "Mark Wheelhouse",
+            number: 3
+          },
+          mostHours: {
+            name: "Joon-Ho Son",
+            number: 50
+          },
+          onTheRise: {
+            name: "Jane Doe",
+            number: 4
+          }
         }
-      })
-      .catch(error => res.status(500).send(error));
-  };
+      });
+    }),
+    (this.updateAvailability = function(req, res) {
+      // Check bearer token id matches parameter id
+      if (req.user.id != req.params.id) {
+        res
+          .status(401)
+          .send({ message: "You can only update your own availability." });
+        return;
+      }
+
+      volunteerRepository
+        .getById(req.params.id)
+        .then(volunteer => {
+          if (!volunteer) {
+            res.status(400).send({ message: "No volunteer with that id" });
+          } else {
+            volunteerRepository
+              .updateAvailability(req.params.id, req.body.availability)
+              .then(result =>
+                res.status(201).send({
+                  message: "Availability Updated Successfuly"
+                })
+              )
+              .catch(error => res.status(400).send(error));
+          }
+        })
+        .catch(error => res.status(500).send(error));
+    });
 
   this.getAvailability = function(req, res) {
     // Check bearer token id matches parameter id
@@ -171,39 +158,56 @@ var VolunteerController = function(volunteerRepository, shiftRepository) {
       })
       .then(bookings => {
         var shiftIds = bookings.map(booking => booking.shiftId);
-        
+
         if (req.query.booked) {
-          return shiftRepository.getAllWithRequirements({
-            id: { [Op.in]: shiftIds }
-          });
-        }
-        
-        return Predictor.computeExpectedShortages()
-          .then(_ => {
-
-            return shiftRepository
-              .getAllWithRequirements({
-                id: { [Op.notIn]: shiftIds }
-              })
-              .then(shifts => {
-                var result = [];
-                shifts.forEach(s => {
-                  var shift = s.toJSON();
-                  if (volunteerIsAvailableForShift(volunteer, shift) > 0.5) {
-                    for (var i = 0; i < shift.requirements.length; i++) {
-                      var requirement = shift.requirements[i];
-                      if (requirement.expectedShortage > EXPECTED_SHORTAGE_THRESHOLD) {
-                        requirement.recommended = true;
-                      }
-                      shift.requirements[i] = requirement;
+          return shiftRepository
+            .getAllWithRequirements({
+              id: { [Op.in]: shiftIds }
+            })
+            .then(shifts => {
+              var result = [];
+              shifts.forEach(s => {
+                var shift = s.toJSON();
+                var requirements = [];
+                shift.requirements.forEach(requirement => {
+                  requirement.bookings.forEach(booking => {
+                    if (booking.volunteerId === volunteer.userId) {
+                      requirements.push(requirement);
                     }
-                  }
-                  result.push(shift);
+                  });
                 });
-                return result;
+                shift.requirements = requirements;
+                result.push(shift);
               });
+              return result;
+            });
+        }
 
-          })
+        return Predictor.computeExpectedShortages().then(_ => {
+          return shiftRepository
+            .getAllWithRequirements({
+              id: { [Op.notIn]: shiftIds }
+            })
+            .then(shifts => {
+              var result = [];
+              shifts.forEach(s => {
+                var shift = s.toJSON();
+                if (volunteerIsAvailableForShift(volunteer, shift) > 0.5) {
+                  for (var i = 0; i < shift.requirements.length; i++) {
+                    var requirement = shift.requirements[i];
+                    if (
+                      requirement.expectedShortage > EXPECTED_SHORTAGE_THRESHOLD
+                    ) {
+                      requirement.recommended = true;
+                    }
+                    shift.requirements[i] = requirement;
+                  }
+                }
+                result.push(shift);
+              });
+              return result;
+            });
+        });
       })
       .then(shifts => res.status(200).send(shifts))
       .catch(err => res.status(500).send(err));
