0d675eca0732d2b40b89b4a5b2aafe13a8038356
William B <wb2117@ic.ac.uk>
2019-06-09 11:12:38 +0100

    Add lastLogin to user. Return lastLogin on login

--- server/models/user.js
+++ server/models/user.js
@@ -1,44 +1,51 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      isAdmin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      telephone: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      dob: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+      },
+      lastLogin: {
+        type: DataTypes.DATE,
+        allowNull: true
+      }
     },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    isAdmin: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    telephone: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    dob: {
-      type: DataTypes.DATEONLY,
-      allowNull: false
-    }
-  }, {});
+    {}
+  );
   User.associate = function(models) {
-
     User.hasOne(models.Volunteer, {
       as: "volunteer",
       foreignKey: "userId"
