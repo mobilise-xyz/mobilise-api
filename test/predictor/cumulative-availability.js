17769b8ba7b1ee574d18a8cf7a973b6714019c7b
Arjun Singh <as15217@ic.ac.uk>
2019-06-11 23:12:02 +0100

    Add tests for cumulative availability

--- test/predictor/cumulative-availability.js
+++ test/predictor/cumulative-availability.js
@@ -4,12 +4,40 @@ var expect = chai.expect;
 
 const Seeded = require('../../server/utils/seeded');
 const Predictor = require('../../server/recommenderSystem').Predictor;
+const volunteerRepository = require('../../server/repositories').VolunteerRepository;
 
-describe('Cumulative Availability', function() {
+describe('Cumulative Availability for Seeded', function() {
 
     it('Returns zeroed array for Seeded Volunteers', async function() {
         var cumulative = await Predictor.getCumulativeAvailability();
 
         expect(cumulative).to.eql(Array(7).fill([0,0,0]));
     })
-})
+});
+
+describe('Cumulative Availability', async function() {
+
+    // Update a volunteer to maximum availability
+    before( async function() {
+        await volunteerRepository.updateAvailability(
+            Seeded.volunteers[0].UUID,
+            Array(7).fill(['2', '2', '2'])
+        );
+    })
+
+    // Set volunteer availability back to default
+    after( async function() {
+        await volunteerRepository.updateAvailability(
+            Seeded.volunteers[0].UUID,
+            Array(7).fill(['0', '0', '0'])
+        );
+    })
+
+    it('Returns array of 1s for single volunteer with maximum availability', async function() {
+        var cumulative = await Predictor.getCumulativeAvailability();
+
+        console.log(cumulative);
+
+        expect(cumulative).to.eql(Array(7).fill([1, 1, 1]));
+    })
+});
