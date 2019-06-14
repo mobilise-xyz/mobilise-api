1bbb566aa251d0a8a3cefd570e024603a2fe86ab
William B <wb2117@ic.ac.uk>
2019-06-14 15:42:19 +0100

    Remove check on key

--- server/controllers/predictionController.js
+++ server/controllers/predictionController.js
@@ -2,16 +2,10 @@ const Predictor = require("../recommenderSystem").Predictor;
 
 var PredictionController = function() {
   this.computeExpectedShortages = function(req, res) {
-    if (
-      process.env.COMPUTATION_TRIGGER_KEY !== req.body.key
-    ) {
-      res.status(401).send({ message: "Unauthorised request" });
-    } else {
       Predictor.computeExpectedShortages().then(res => {
         res.status(200).send({ message: "Computation Successful" });
       });
     }
-  };
 };
 
 module.exports = new PredictionController();
