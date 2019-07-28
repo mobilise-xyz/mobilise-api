26ed553f5b2d4b7b96cc536b65149f3c6cad6f0b
William B <wb2117@ic.ac.uk>
2019-07-29 00:31:51 +0100

    Remove calendar route for now

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -2,7 +2,4 @@ let express = require("express");
 let router = express.Router();
 let controller = require("../controllers").CalendarController;
 
-/* Get calendar for a volunteer's shifts */
-router.get("/:id/bookings.ics", controller.subscribe);
-
 module.exports = router;
