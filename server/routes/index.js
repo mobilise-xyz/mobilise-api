8201cf911ddba35456f70335ae2e2fd7037f654f
Joon-Ho Son <json0820@gmail.com>
2019-08-03 13:26:53 +0100

    Remove res.render() which was breaking since no view engine was specified.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.send('the beans are broken');
 });
 
 module.exports = router;
