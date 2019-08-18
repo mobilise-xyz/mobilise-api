6cffdcc1accce1fb77a18135a957350cbfb71334
William B <wb2117@ic.ac.uk>
2019-08-18 16:17:22 +0100

    Fix the migrated type for user approved

--- server/migrations/20190809155639-add-approved-to-user.js
+++ server/migrations/20190809155639-add-approved-to-user.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
     return queryInterface.addColumn("Users", "approved", {
-      type: Sequelize.STRING,
+      type: Sequelize.BOOLEAN,
       allowNull: false,
       defaultValue: false
     })
